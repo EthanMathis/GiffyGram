2e7672b7497ed6bc74379a0b06b34f055f771a36
EthanMathis <ethan.mathis@outlook.com>
2021-03-24 14:12:36 -0500

    user registration and login/logout functioning

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -13,14 +13,15 @@ export const usePostCollection = () => {
 }
 
 export const getPosts = () => {
-  return fetch("http://localhost:8088/posts")
+  const userId = getLoggedInUser().id
+  return fetch(`http://localhost:8088/posts?_expand=user`)
     .then(response => response.json())
     .then(parsedResponse => {
+      console.log("data with user", parsedResponse)
       postCollection = parsedResponse
       return parsedResponse;
     })
 }
-
 export const createPost = postObj => {
   return fetch("http://localhost:8088/posts", {
       method: "POST",
@@ -33,12 +34,77 @@ export const createPost = postObj => {
       .then(response => response.json())
 }
 
-const loggedInUser = {
-	id: 3,
-	name: "Bryan",
-	email: "bryan@home.com"
+export const deletePost = postId => {
+  return fetch(`http://localhost:8088/posts/${postId}`, {
+      method: "DELETE",
+      headers: {
+          "Content-Type": "application/json"
+      }
+
+  })
+      .then(response => response.json())
+      .then(getPosts)
 }
+
+export const getSinglePost = (postId) => {
+  return fetch(`http://localhost:8088/posts/${postId}`)
+    .then(response => response.json())
+}
+
+export const updatePost = postObj => {
+  return fetch(`http://localhost:8088/posts/${postObj.id}`, {
+      method: "PUT",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(postObj)
+
+  })
+      .then(response => response.json())
+      .then(getPosts)
+}
+
+let loggedInUser = {};
 
 export const getLoggedInUser = () => {
 	return {...loggedInUser};
+}
+
+export const logoutUser = () => {
+  loggedInUser = {}
+}
+
+export const setLoggedInUser = (userObj) => {
+  loggedInUser = userObj;
+}
+
+export const loginUser = (userObj) => {
+  return fetch(`http://localhost:8088/users?name=${userObj.name}&email=${userObj.email}`)
+  .then(response => response.json())
+  .then(parsedUser => {
+    //is there a user?
+    console.log("parsedUser", parsedUser) //data is returned as an array
+    if (parsedUser.length > 0){
+      setLoggedInUser(parsedUser[0]);
+      return getLoggedInUser();
+    }else {
+      //no user
+      return false;
+    }
+  })
+}
+
+export const registerUser = (userObj) => {
+  return fetch(`http://localhost:8088/users`, {
+    method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userObj)
+  })
+  .then(response => response.json())
+  .then(parsedUser => {
+    setLoggedInUser(parsedUser);
+    return getLoggedInUser();
+  })
 }
--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -1,10 +1,28 @@
+import { getLoggedInUser } from "../data/DataManager.js"
+
 export const Post = (postObject) => {
+  const currentUser = getLoggedInUser().id
+  if (currentUser === postObject.userId) {
     return `
       <section class="post">
         <header>
+        <h3 class="post__author">Posted By: ${postObject.user.name}</h3>
             <h2 class="post__title">${postObject.title}</h2>
         </header>
         <img class="post__image" src="${postObject.imageURL}" />
+        <button id="delete__${postObject.id}">Delete</button>
+        <button id="edit__${postObject.id}">Edit</button>
       </section>
     `
+  } else {
+    return `
+    <section class="post">
+      <header>
+      <h3 class="post__author">Posted By: ${postObject.user.name}</h3>
+          <h2 class="post__title">${postObject.title}</h2>
+      </header>
+      <img class="post__image" src="${postObject.imageURL}" />
+    </section>
+  `
+  }
   }
